41058c25a2f22b69432408820f9109089bb9e8f4
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-05 22:05:27 +0200

    feat: implement basic functionality for running tests

--- addons/confirma/src/scenes/test_runner/TestRunner.cs
+++ addons/confirma/src/scenes/test_runner/TestRunner.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using Confirma.Helpers;
 using Godot;
 
 namespace Confirma.TestRunner;
@@ -11,5 +13,32 @@ public partial class TestRunner : Control
 	public override void _Ready()
 	{
 		_output = GetNode<RichTextLabel>("%Output");
+
+		var assembly = Assembly.GetExecutingAssembly();
+		var testClasses = Reflection.GetTestClassesFromAssembly(assembly);
+
+		_output.Text = $"Running {testClasses.Length} tests...\n";
+
+		foreach (var testClass in testClasses)
+		{
+			var methods = Reflection.GetTestMethodsFromType(testClass);
+
+			_output.AppendText($"Running tests for {testClass.Name}...\n");
+
+			foreach (var method in methods)
+			{
+				_output.AppendText($"\t| Running {method.Name}...\n");
+
+				try
+				{
+					method.Invoke(null, null);
+					_output.AppendText("\t| -\t[color=green]Passed.[/color]\n");
+				}
+				catch (TargetInvocationException e)
+				{
+					_output.AppendText($"\t| -\t[color=red]Failed: {e.InnerException?.Message}[/color]\n");
+				}
+			}
+		}
 	}
 }
