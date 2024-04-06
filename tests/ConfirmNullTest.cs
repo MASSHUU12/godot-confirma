154774d52b120807a6f996ff232473322bdf26ee
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-06 18:52:16 +0200

    test: update ConfirmNullTest

--- tests/ConfirmNullTest.cs
+++ tests/ConfirmNullTest.cs
@@ -5,17 +5,20 @@ namespace Confirma.Tests;
 [TestClass]
 public static class ConfirmNullTest
 {
-	[TestCase(parameters: "Lorem ipsum")]
 	[TestCase(parameters: new object?[] { null })]
-	public static void ConfirmNull_Null(string? actual)
+	public static void ConfirmNull_WhenNull(object? actual)
 	{
 		actual.ConfirmNull();
 	}
 
+	// TODO: Create a test case for ConfirmNull_WhenNotNull
+
 	[TestCase(parameters: "Lorem ipsum")]
-	[TestCase(parameters: new object?[] { null })]
-	public static void ConfirmNull_NotNull(string? actual)
+	[TestCase(parameters: 2)]
+	public static void ConfirmNotNull_WhenNotNull(object? actual)
 	{
-		actual.ConfirmNull();
+		actual.ConfirmNotNull();
 	}
+
+	// TODO: Create a test case for ConfirmNotNull_WhenNull
 }
