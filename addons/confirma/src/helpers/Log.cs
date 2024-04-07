8eac2c4fba7b8557aabd7c8ef8cfd8377d1f2e06
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-07 17:15:44 +0200

    feat: create Log class

---
+++ addons/confirma/src/helpers/Log.cs
@@ -0,0 +1,56 @@
+using Godot;
+
+namespace Confirma.Helpers;
+
+public class Log
+{
+#nullable disable
+	private readonly RichTextLabel _richOutput;
+#nullable restore
+
+	private readonly bool _headless = false;
+
+	public Log(RichTextLabel? richOutput)
+	{
+		_richOutput = richOutput;
+	}
+
+	public Log()
+	{
+		_richOutput = null;
+		_headless = true;
+	}
+
+	public void Print(string message)
+	{
+		if (_headless) _richOutput.AppendText(message);
+		else GD.Print(message);
+	}
+
+	public void PrintLine(string message)
+	{
+		Print($"{message}\n");
+	}
+
+	public void PrintError(string message)
+	{
+		Print(ColorText(message, "red"));
+	}
+
+	public void PrintSuccess(string message)
+	{
+		Print(ColorText(message, "green"));
+	}
+
+	public void PrintWarning(string message)
+	{
+		Print(ColorText(message, "yellow"));
+	}
+
+	private string ColorText(string text, string color)
+	{
+		var c = new Colors(color);
+
+		return _headless ? c.ToTerminal(text) : c.ToGodot(text);
+	}
+}
