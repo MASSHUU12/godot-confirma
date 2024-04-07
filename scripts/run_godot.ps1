5985b0dee7bc607345a87f5fc69fa2d91f0bd6f6
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-07 13:14:57 +0200

    feat: create PowerShell script to run Godot with passed arguments

---
+++ scripts/run_godot.ps1
@@ -0,0 +1,16 @@
+param (
+	[Parameter(ValueFromRemainingArguments = $true)]
+	[string]$params = ""
+)
+
+if (-not $env:GODOT) {
+	Write-Host "GODOT environment variable not set"
+	exit 1
+}
+
+if (-not (Test-Path $env:GODOT -ErrorAction SilentlyContinue)) {
+	Write-Host "Godot executable not found at $env:GODOT"
+	exit 1
+}
+
+& $env:GODOT ./ -- $params
