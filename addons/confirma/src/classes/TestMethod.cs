5d2c022eed083816fbf8c457afee8965ca8a3857
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-08 13:38:50 +0200

    feat: store name of the method

--- addons/confirma/src/classes/TestMethod.cs
+++ addons/confirma/src/classes/TestMethod.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Reflection;
+using Confirma.Attributes;
 
 namespace Confirma.Classes;
 
@@ -7,10 +8,12 @@ public class TestMethod
 {
 	public MethodInfo Method { get; }
 	public IEnumerable<TestCase> TestCases { get; }
+	public string Name { get; }
 
 	public TestMethod(MethodInfo method)
 	{
 		Method = method;
 		TestCases = TestDiscovery.DiscoverTestCases(method);
+		Name = Method.GetCustomAttribute<TestNameAttribute>()?.Name ?? Method.Name;
 	}
 }
