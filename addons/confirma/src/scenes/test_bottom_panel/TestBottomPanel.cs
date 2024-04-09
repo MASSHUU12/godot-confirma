2990a7f45237eccb01ab7c77df3e4ff12ad96279
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-09 10:45:36 +0200

    feat: add Clear Output button

--- addons/confirma/src/scenes/test_bottom_panel/TestBottomPanel.cs
+++ addons/confirma/src/scenes/test_bottom_panel/TestBottomPanel.cs
@@ -8,14 +8,18 @@ namespace Confirma.Scenes;
 public partial class TestBottomPanel : Control
 {
 #nullable disable
-	private Button _runAllTestsButton;
+	private Button _runAllTests;
+	private Button _clearOutput;
 	private TestRunnerEditor _testRunner;
 #nullable restore
 
 	public override void _Ready()
 	{
-		_runAllTestsButton = GetNode<Button>("%RunAllTests");
-		_runAllTestsButton.Pressed += OnRunAllTestsPressed;
+		_runAllTests = GetNode<Button>("%RunAllTests");
+		_runAllTests.Pressed += OnRunAllTestsPressed;
+
+		_clearOutput = GetNode<Button>("%ClearOutput");
+		_clearOutput.Pressed += OnClearOutputPressed;
 
 		_testRunner = GetNode<TestRunnerEditor>("%TestRunnerEditor");
 	}
@@ -23,6 +27,11 @@ public partial class TestBottomPanel : Control
 	private void OnRunAllTestsPressed()
 	{
 		_testRunner.RunAllTests();
+	}
+
+	private void OnClearOutputPressed()
+	{
+		_testRunner.ClearOutput();
 	}
 }
 
--- addons/confirma/src/scenes/test_runner_editor/TestRunnerEditor.cs
+++ addons/confirma/src/scenes/test_runner_editor/TestRunnerEditor.cs
@@ -1,5 +1,6 @@
 #if TOOLS
 
+using System;
 using Confirma.Helpers;
 using Godot;
 
@@ -12,8 +13,14 @@ public partial class TestRunnerEditor : TestRunner
 	{
 		base._Ready();
 
-		_output.Clear();
 		_executor = new(new Log(_output), new(false));
+
+		ClearOutput();
+	}
+
+	public void ClearOutput()
+	{
+		_output.Clear();
 	}
 }
 
