a90a928ee34cb77d9550c2220e799799ab3b405a
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-10 15:47:33 +0200

    feat: add ConfirmEqual & ConfirmNotEqual overloads for arrays

--- tests/ConfirmEqualTest.cs
+++ tests/ConfirmEqualTest.cs
@@ -15,11 +15,23 @@ public static class ConfirmEqualTest
 		o1.ConfirmEqual(o2);
 	}
 
+	[TestCase(new string[] { "Hello,", "world!" }, new string[] { "Hello,", "world!" })]
+	public static void ConfirmEqual_WhenArrEqual(object[] o1, object[] o2)
+	{
+		o1.ConfirmEqual(o2);
+	}
+
 	[TestCase(1, 2)]
 	[TestCase("Lorem ipsum", "Dolor sit amet")]
 	[TestCase(2d, 3d)]
 	[TestCase(2f, 2)]
 	public static void ConfirmEqual_WhenNotEqual(object o1, object o2)
+	{
+		Confirm.ConfirmThrows<ConfirmAssertException>(() => o1.ConfirmEqual(o2));
+	}
+
+	[TestCase(new string[] { "Hello,", "world!" }, new string[] { "Hi,", "world!" })]
+	public static void ConfirmEqual_WhenArrNotEqual(object[] o1, object[] o2)
 	{
 		Confirm.ConfirmThrows<ConfirmAssertException>(() => o1.ConfirmEqual(o2));
 	}
@@ -34,12 +46,23 @@ public static class ConfirmEqualTest
 		o1.ConfirmNotEqual(o2);
 	}
 
-	// TODO: Create a test case for ConfirmNotEqual_WhenEqual
+	[TestCase(new string[] { "Hello,", "world!" }, new string[] { "Hi,", "world!" })]
+	public static void ConfirmNotEqual_WhenArrNotEqual(object[] o1, object[] o2)
+	{
+		o1.ConfirmNotEqual(o2);
+	}
+
 	[TestCase(1, 1)]
 	[TestCase("Lorem ipsum", "Lorem ipsum")]
 	[TestCase(2d, 2d)]
 	[TestCase(2f, 2f)]
 	public static void ConfirmNotEqual_WhenEqual(object o1, object o2)
+	{
+		Confirm.ConfirmThrows<ConfirmAssertException>(() => o1.ConfirmNotEqual(o2));
+	}
+
+	[TestCase(new string[] { "Hello,", "world!" }, new string[] { "Hello,", "world!" })]
+	public static void ConfirmNotEqual_WhenArrEqual(object[] o1, object[] o2)
 	{
 		Confirm.ConfirmThrows<ConfirmAssertException>(() => o1.ConfirmNotEqual(o2));
 	}
