bf7d0aa0726ace4a717d5e867d000d1da2d20acd
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-10 16:26:57 +0200

    feat: create ConfirmArrayExtension and its tests

---
+++ tests/ConfirmArrayTest.cs
@@ -0,0 +1,54 @@
+using System;
+using Confirma.Attributes;
+
+namespace Confirma.Tests;
+
+[TestClass]
+public static class ConfirmArrayTest
+{
+	[TestCase(new int[] { }, 0)]
+	[TestCase(new int[] { 1 }, 1)]
+	[TestCase(new int[] { 1, 2 }, 2)]
+	public static void ConfirmSize_WhenIsOfSize(int[] array, int expectedSize)
+	{
+		array.ConfirmSize(expectedSize);
+	}
+
+	[TestCase(new int[] { 1 }, 0)]
+	[TestCase(new int[] { 1, 2 }, 1)]
+	[TestCase(new int[] { 1, 2, 3 }, 2)]
+	public static void ConfirmSize_WhenIsNotOfSize(int[] array, int expectedSize)
+	{
+		Confirm.ConfirmThrows<ConfirmAssertException>(() => array.ConfirmSize(expectedSize));
+	}
+
+	[TestCase]
+	public static void ConfirmEmpty_WhenIsEmpty()
+	{
+		Array.Empty<int>().ConfirmEmpty();
+	}
+
+	[TestCase(new int[] { 1 })]
+	[TestCase(new int[] { 1, 2 })]
+	[TestCase(new int[] { 1, 2, 3 })]
+	public static void ConfirmEmpty_WhenIsNotEmpty(int[] array)
+	{
+		Confirm.ConfirmThrows<ConfirmAssertException>(() => array.ConfirmEmpty());
+	}
+
+	[TestCase(new int[] { 1 }, 1)]
+	[TestCase(new int[] { 1, 2 }, 2)]
+	[TestCase(new int[] { 1, 2, 3 }, 2)]
+	public static void ConfirmContains_WhenContains(int[] array, int expected)
+	{
+		array.ConfirmContains(expected);
+	}
+
+	[TestCase(new int[] { 1 }, 2)]
+	[TestCase(new int[] { 1, 2 }, 3)]
+	[TestCase(new int[] { 1, 2, 3 }, 4)]
+	public static void ConfirmContains_WhenNotContains(int[] array, int expected)
+	{
+		Confirm.ConfirmThrows<ConfirmAssertException>(() => array.ConfirmContains(expected));
+	}
+}
