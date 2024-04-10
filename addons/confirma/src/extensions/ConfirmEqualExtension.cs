ad54902e5a54c83546b623f0ffb2da081663f3e3
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-10 11:22:50 +0200

    feat: allow nullable values

--- addons/confirma/src/extensions/ConfirmEqualExtension.cs
+++ addons/confirma/src/extensions/ConfirmEqualExtension.cs
@@ -2,15 +2,15 @@ namespace Confirma;
 
 public static class ConfirmEqualExtension
 {
-	public static void ConfirmEqual<T>(this T actual, T expected, string? message = null)
+	public static void ConfirmEqual<T>(this T? actual, T? expected, string? message = null)
 	{
-		if (!actual?.Equals(expected) ?? false)
+		if (!(actual?.Equals(expected)) ?? false)
 		{
 			throw new ConfirmAssertException(message ?? $"Expected {expected} but was {actual}.");
 		}
 	}
 
-	public static void ConfirmNotEqual<T>(this T actual, T expected, string? message = null)
+	public static void ConfirmNotEqual<T>(this T? actual, T? expected, string? message = null)
 	{
 		if (actual?.Equals(expected) ?? false)
 		{
