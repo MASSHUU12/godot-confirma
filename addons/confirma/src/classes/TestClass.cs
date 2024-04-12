690602c2d93fc755a958bd7625b6ca302c1db91e
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-12 12:27:08 +0200

    feat: handle BeforeAll attribute

--- addons/confirma/src/classes/TestClass.cs
+++ addons/confirma/src/classes/TestClass.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
+using Confirma.Attributes;
 using Confirma.Helpers;
 using Confirma.Types;
 
@@ -10,15 +12,38 @@ public class TestClass
 	public Type Type { get; }
 	public IEnumerable<TestMethod> TestMethods { get; }
 
+	private MethodInfo? _beforeAllMethod = null;
+	private bool _hasMoreBeforeAll = false;
+
 	public TestClass(Type type)
 	{
 		Type = type;
 		TestMethods = TestDiscovery.DiscoverTestMethods(type);
+
+		InitialLookup();
 	}
 
 	public TestClassResult Run(Log log)
 	{
 		uint passed = 0, failed = 0, ignored = 0;
+
+		if (_hasMoreBeforeAll) log.PrintWarning(
+			$"Multiple [BeforeAll] methods found in {Type.Name}. Running only the first one.\n"
+		);
+
+		if (_beforeAllMethod is not null)
+		{
+			log.Print($"[BeforeAll] {Type.Name}");
+
+			try
+			{
+				_beforeAllMethod.Invoke(null, null);
+			}
+			catch (ConfirmAssertException e)
+			{
+				log.PrintError($"- {e.Message}\n");
+			}
+		}
 
 		foreach (var method in TestMethods)
 		{
@@ -30,5 +55,16 @@ public class TestClass
 		}
 
 		return new(passed, failed, ignored);
+	}
+
+	private void InitialLookup()
+	{
+		foreach (var method in Reflection.GetMethodsWithAttribute<BeforeAllAttribute>(Type))
+		{
+			if (method.GetCustomAttribute<BeforeAllAttribute>() is null) continue;
+
+			if (_beforeAllMethod is null) _beforeAllMethod = method;
+			else _hasMoreBeforeAll = true;
+		}
 	}
 }
