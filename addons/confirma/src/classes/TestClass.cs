fe0783ca6e3c1110c7d95fd6515d68c3f2f29976
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-17 19:03:02 +0200

    refactor: TestClass

--- addons/confirma/src/classes/TestClass.cs
+++ addons/confirma/src/classes/TestClass.cs
@@ -12,17 +12,7 @@ public class TestClass
 	public Type Type { get; }
 	public IEnumerable<TestMethod> TestMethods { get; }
 
-	private MethodInfo? _beforeAllMethod = null;
-	private bool _hasMoreBeforeAll = false;
-
-	private MethodInfo? _afterAllMethod = null;
-	private bool _hasMoreAfterAll = false;
-
-	private MethodInfo? _setUpMethod = null;
-	private bool _hasMoreSetUp = false;
-
-	private MethodInfo? _tearDownMethod = null;
-	private bool _hasMoreTearDown = false;
+	private readonly Dictionary<string, LifecycleMethodData> _lifecycleMethods = new();
 
 	public TestClass(Type type)
 	{
@@ -36,21 +26,22 @@ public class TestClass
 	{
 		uint passed = 0, failed = 0, ignored = 0, warnings = 0;
 
-		if (_beforeAllMethod is not null) warnings += RunBeforeAll();
-		if (_afterAllMethod is not null) warnings += RunAfterAll();
+		warnings += RunLifecycleMethod("BeforeAll");
 
 		foreach (var method in TestMethods)
 		{
-			if (_setUpMethod is not null) warnings += RunSetUp();
+			warnings += RunLifecycleMethod("SetUp");
 
 			var methodResult = method.Run();
 
-			if (_tearDownMethod is not null) warnings += RunTearDown();
+			warnings += RunLifecycleMethod("TearDown");
 
 			passed += methodResult.TestsPassed;
 			failed += methodResult.TestsFailed;
 			ignored += methodResult.TestsIgnored;
 		}
+
+		warnings += RunLifecycleMethod("AfterAll");
 
 		return new(passed, failed, ignored, warnings);
 	}
@@ -61,87 +52,51 @@ public class TestClass
 		{
 			if (method.GetCustomAttribute<BeforeAllAttribute>() is null) continue;
 
-			if (_beforeAllMethod is null) _beforeAllMethod = method;
-			else _hasMoreBeforeAll = true;
+			if (!_lifecycleMethods.TryGetValue("BeforeAll", out var beforeAll))
+				_lifecycleMethods.Add("BeforeAll", new(method, "BeforeAll", false));
+			else beforeAll.HasMultiple = true;
 		}
 
 		foreach (var method in Reflection.GetMethodsWithAttribute<AfterAllAttribute>(Type))
 		{
-			if (method.GetCustomAttribute<AfterAllAttribute>() is null) continue;
-
-			if (_afterAllMethod is null) _afterAllMethod = method;
-			else _hasMoreAfterAll = true;
+			if (!_lifecycleMethods.TryGetValue("AfterAll", out var afterAll))
+				_lifecycleMethods.Add("AfterAll", new(method, "AfterAll", false));
+			else afterAll.HasMultiple = true;
 		}
 
 		foreach (var method in Reflection.GetMethodsWithAttribute<SetUpAttribute>(Type))
 		{
-			if (method.GetCustomAttribute<SetUpAttribute>() is null) continue;
-
-			if (_setUpMethod is null) _setUpMethod = method;
-			else _hasMoreSetUp = true;
+			if (!_lifecycleMethods.TryGetValue("SetUp", out var setUp))
+				_lifecycleMethods.Add("SetUp", new(method, "SetUp", false));
+			else setUp.HasMultiple = true;
 		}
 
 		foreach (var method in Reflection.GetMethodsWithAttribute<TearDownAttribute>(Type))
 		{
-			if (method.GetCustomAttribute<TearDownAttribute>() is null) continue;
-
-			if (_tearDownMethod is null) _tearDownMethod = method;
-			else _hasMoreTearDown = true;
+			if (!_lifecycleMethods.TryGetValue("TearDown", out var tearDown))
+				_lifecycleMethods.Add("TearDown", new(method, "TearDown", false));
+			else tearDown.HasMultiple = true;
 		}
 	}
 
-	private byte RunLifecycleMethod(MethodInfo method, string name, bool hasMultiple)
+	private byte RunLifecycleMethod(string name)
 	{
-		if (hasMultiple)
+		if (!_lifecycleMethods.TryGetValue(name, out var method)) return 0;
+
+		if (method.HasMultiple)
 			Log.PrintWarning($"Multiple [{name}] methods found in {Type.Name}. Running only the first one.\n");
 
 		Log.PrintLine($"[{name}] {Type.Name}");
 
 		try
 		{
-			method.Invoke(null, null);
+			method.Method.Invoke(null, null);
 		}
 		catch (Exception e)
 		{
 			Log.PrintError($"- {e.Message}");
 		}
 
-		return hasMultiple ? (byte)1 : (byte)0;
-	}
-
-	private byte RunBeforeAll()
-	{
-		return RunLifecycleMethod(
-		  _beforeAllMethod!,
-		  "BeforeAll",
-		  _hasMoreBeforeAll
-		);
-	}
-
-	private byte RunAfterAll()
-	{
-		return RunLifecycleMethod(
-		 _afterAllMethod!,
-		 "AfterAll",
-		 _hasMoreAfterAll
-		);
-	}
-
-	private byte RunSetUp()
-	{
-		return RunLifecycleMethod(
-			_setUpMethod!,
-			"SetUp",
-			_hasMoreSetUp
-		);
-	}
-
-	private byte RunTearDown()
-	{
-		return RunLifecycleMethod(
-			_tearDownMethod!,
-			"TearDown",
-			_hasMoreTearDown
-		);
+		return method.HasMultiple ? (byte)1 : (byte)0;
 	}
 }
