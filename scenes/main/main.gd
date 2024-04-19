8b3191d47a930500a53430f7d5ee05bc001501a3
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-19 13:43:42 +0200

    chore: update UID to the scene in main.gd

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -2,10 +2,8 @@ extends Control
 
 @onready var btn: Button = get_node("CenterContainer/Panel/MarginContainer/VBoxContainer/Button")
 
-
 func _ready() -> void:
 	btn.connect("pressed", _on_button_pressed)
 
-
 func _on_button_pressed():
-	get_tree().change_scene_to_packed(load("uid://cqbaquyf403ot"))
+	get_tree().change_scene_to_packed(load("uid://cq76c14wl2ti3"))
