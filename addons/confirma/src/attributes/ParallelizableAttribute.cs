3986b1bb85a6b64bec05cc5a287d17722fd9798a
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-22 19:20:45 +0200

    feat: create Parallelizable attribute

---
+++ addons/confirma/src/attributes/ParallelizableAttribute.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Confirma.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class ParallelizableAttribute : Attribute
+{
+	public ushort Threads { get; }
+
+	public ParallelizableAttribute()
+	{
+		Threads = Math.Max((ushort)2, (ushort)Environment.ProcessorCount);
+	}
+
+	public ParallelizableAttribute(ushort threads)
+	{
+		Threads = ValidateThreads(threads);
+	}
+
+	private static ushort ValidateThreads(ushort threads)
+	{
+		return Math.Min(
+			Math.Max((ushort)1, threads),
+			(ushort)Environment.ProcessorCount
+		);
+	}
+}
