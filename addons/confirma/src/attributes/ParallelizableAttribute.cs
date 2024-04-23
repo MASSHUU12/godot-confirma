f90d7c1648d7e1d17b0f73c28272e1dd2bd3f2f3
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-23 11:59:11 +0200

    feat: remove option to specify number of threads

--- addons/confirma/src/attributes/ParallelizableAttribute.cs
+++ addons/confirma/src/attributes/ParallelizableAttribute.cs
@@ -5,23 +5,5 @@ namespace Confirma.Attributes;
 [AttributeUsage(AttributeTargets.Class)]
 public class ParallelizableAttribute : Attribute
 {
-	public ushort Threads { get; }
-
-	public ParallelizableAttribute()
-	{
-		Threads = Math.Min((ushort)2, (ushort)Environment.ProcessorCount);
-	}
-
-	public ParallelizableAttribute(ushort threads)
-	{
-		Threads = ValidateThreads(threads);
-	}
-
-	private static ushort ValidateThreads(ushort threads)
-	{
-		return Math.Min(
-			Math.Max((ushort)1, threads),
-			(ushort)Environment.ProcessorCount
-		);
-	}
+	public ParallelizableAttribute() { }
 }
