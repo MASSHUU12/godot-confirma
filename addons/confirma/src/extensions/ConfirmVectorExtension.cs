80fd683c1a2383bdd249e50d3174a898e79bd538
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-29 21:29:27 +0200

    feat: create ConfirmEqualApprox & ConfirmNotEqualApprox for vectors

---
+++ addons/confirma/src/extensions/ConfirmVectorExtension.cs
@@ -0,0 +1,53 @@
+using Confirma.Exceptions;
+using Godot;
+
+namespace Confirma;
+
+public static class ConfirmVectorExtension
+{
+	#region ConfirmEqualApprox
+	public static void ConfirmEqualApprox(this Vector2 vector, Vector2 expected, float tolerance = 0.0001f, string? message = null)
+	{
+		if (vector.IsEqualApprox(expected, tolerance)) return;
+
+		throw new ConfirmAssertException(message ?? $"Expected {expected}, but got {vector}");
+	}
+
+	public static void ConfirmEqualApprox(this Vector3 vector, Vector3 expected, float tolerance = 0.0001f, string? message = null)
+	{
+		if (vector.IsEqualApprox(expected, tolerance)) return;
+
+		throw new ConfirmAssertException(message ?? $"Expected {expected}, but got {vector}");
+	}
+
+	public static void ConfirmEqualApprox(this Vector4 vector, Vector4 expected, float tolerance = 0.0001f, string? message = null)
+	{
+		if (vector.IsEqualApprox(expected, tolerance)) return;
+
+		throw new ConfirmAssertException(message ?? $"Expected {expected}, but got {vector}");
+	}
+	#endregion
+
+	#region ConfirmNotEqualApprox
+	public static void ConfirmNotEqualApprox(this Vector2 vector, Vector2 expected, float tolerance = 0.0001f, string? message = null)
+	{
+		if (!vector.IsEqualApprox(expected, tolerance)) return;
+
+		throw new ConfirmAssertException(message ?? $"Expected not {expected}, but got {vector}");
+	}
+
+	public static void ConfirmNotEqualApprox(this Vector3 vector, Vector3 expected, float tolerance = 0.0001f, string? message = null)
+	{
+		if (!vector.IsEqualApprox(expected, tolerance)) return;
+
+		throw new ConfirmAssertException(message ?? $"Expected not {expected}, but got {vector}");
+	}
+
+	public static void ConfirmNotEqualApprox(this Vector4 vector, Vector4 expected, float tolerance = 0.0001f, string? message = null)
+	{
+		if (!vector.IsEqualApprox(expected, tolerance)) return;
+
+		throw new ConfirmAssertException(message ?? $"Expected not {expected}, but got {vector}");
+	}
+	#endregion
+}
