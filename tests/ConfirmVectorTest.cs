66c2dfefb303c9af688a362135c0371f41ebf391
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-29 21:45:10 +0200

    test: write basic tests for ConfirmVector extension

---
+++ tests/ConfirmVectorTest.cs
@@ -0,0 +1,32 @@
+using Godot;
+using Confirma.Attributes;
+using Confirma.Exceptions;
+
+namespace Confirma.Tests;
+
+[TestClass]
+[Parallelizable]
+public static class ConfirmVectorTest
+{
+	[TestCase(1f, 2f, 1f, 2f, 0.0001f)]
+	[TestCase(1f, 2f, 1.00001f, 2.00001f, 0.0001f)]
+	[TestCase(1f, 1f, 1.3f, 1.3f, 0.5f)]
+	public static void ConfirmEqualApprox_WhenEqual(float a, float b, float c, float d, float tolerance)
+	{
+		var vector = new Vector2(a, b);
+		var vector2 = new Vector2(c, d);
+
+		vector.ConfirmEqualApprox(vector2, tolerance);
+	}
+
+	[TestCase(1f, 2f, 1.1f, 2.1f, 0.0001f)]
+	[TestCase(1f, 2f, 1.0001f, 2.0001f, 0.00001f)]
+	[TestCase(1f, 1f, 1.3f, 1.3f, 0.1f)]
+	public static void ConfirmEqualApprox_WhenNotEqual(float a, float b, float c, float d, float tolerance)
+	{
+		var vector = new Vector2(a, b);
+		var vector2 = new Vector2(c, d);
+
+		Confirm.ConfirmThrows<ConfirmAssertException>(() => vector.ConfirmEqualApprox(vector2, tolerance));
+	}
+}
