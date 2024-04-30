c46220aedebbd0a98e96ca58ab4867e1f026ae47
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-30 09:34:14 +0200

    test: write tests for ConfirmNotEqualApprox

--- tests/ConfirmVectorTest.cs
+++ tests/ConfirmVectorTest.cs
@@ -8,6 +8,7 @@ namespace Confirma.Tests;
 [Parallelizable]
 public static class ConfirmVectorTest
 {
+	#region ConfirmEqualApprox
 	[TestCase(1f, 2f, 1f, 2f, 0.0001f)]
 	[TestCase(1f, 2f, 1.00001f, 2.00001f, 0.0001f)]
 	[TestCase(1f, 1f, 1.3f, 1.3f, 0.5f)]
@@ -29,4 +30,29 @@ public static class ConfirmVectorTest
 
 		Confirm.ConfirmThrows<ConfirmAssertException>(() => vector.ConfirmEqualApprox(vector2, tolerance));
 	}
+	#endregion
+
+	#region ConfirmNotEqualApprox
+	[TestCase(1f, 2f, 1.1f, 2.1f, 0.0001f)]
+	[TestCase(1f, 2f, 1.0001f, 2.0001f, 0.00001f)]
+	[TestCase(1f, 1f, 1.3f, 1.3f, 0.1f)]
+	public static void ConfirmNotEqualApprox_WhenNotEqual(float a, float b, float c, float d, float tolerance)
+	{
+		var vector = new Vector2(a, b);
+		var vector2 = new Vector2(c, d);
+
+		vector.ConfirmNotEqualApprox(vector2, tolerance);
+	}
+
+	[TestCase(1f, 2f, 1f, 2f, 0.0001f)]
+	[TestCase(1f, 2f, 1.00001f, 2.00001f, 0.0001f)]
+	[TestCase(1f, 1f, 1.3f, 1.3f, 0.5f)]
+	public static void ConfirmNotEqualApprox_WhenEqual(float a, float b, float c, float d, float tolerance)
+	{
+		var vector = new Vector2(a, b);
+		var vector2 = new Vector2(c, d);
+
+		Confirm.ConfirmThrows<ConfirmAssertException>(() => vector.ConfirmNotEqualApprox(vector2, tolerance));
+	}
+	#endregion
 }
