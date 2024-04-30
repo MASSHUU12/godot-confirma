26b1b6673bbc5106b46f2785b9eef6272f60ba77
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-30 13:13:01 +0200

    docs: add remarks to ConfirmIsPositive & ConfirmIsNegative methods

--- addons/confirma/src/extensions/ConfirmNumericExtension.cs
+++ addons/confirma/src/extensions/ConfirmNumericExtension.cs
@@ -5,45 +5,59 @@ namespace Confirma;
 
 public static class ConfirmNumericExtension
 {
+	#region ConfirmIsPositive
+	/// <remarks>
+	/// This extension method can be used to validate that a value is positive.
+	/// <b>Zero is not considered positive.</b>
+	/// </remarks>
 	public static void ConfirmIsPositive<T>(this T actual, string? message = null)
 	where T : IComparable, IConvertible, IComparable<T>, IEquatable<T>
 	{
-		if (actual.CompareTo((T)Convert.ChangeType(0, typeof(T))) <= 0)
-			throw new ConfirmAssertException(message ?? "Expected object to be positive.");
+		if (actual.CompareTo((T)Convert.ChangeType(0, typeof(T))) > 0) return;
+		throw new ConfirmAssertException(message ?? "Expected object to be positive.");
 	}
 
 	public static void ConfirmIsNotPositive<T>(this T actual, string? message = null)
 	where T : IComparable, IConvertible, IComparable<T>, IEquatable<T>
 	{
-		if (actual.CompareTo((T)Convert.ChangeType(0, typeof(T))) > 0)
-			throw new ConfirmAssertException(message ?? "Expected object to not be positive.");
+		if (actual.CompareTo((T)Convert.ChangeType(0, typeof(T))) <= 0) return;
+		throw new ConfirmAssertException(message ?? "Expected object to not be posi	/// ive.");
 	}
+	#endregion
 
+	#region ConfirmIsNegative
+	/// <remarks>
+	/// This extension method can be used to validate that a value is negative.
+	/// <b>Zero is not considered negative.</b>
+	/// </remarks>
 	public static void ConfirmIsNegative<T>(this T actual, string? message = null)
 	where T : IComparable, IConvertible, IComparable<T>, IEquatable<T>
 	{
-		if (actual.CompareTo((T)Convert.ChangeType(0, typeof(T))) >= 0)
-			throw new ConfirmAssertException(message ?? "Expected object to be negative.");
+		if (actual.CompareTo((T)Convert.ChangeType(0, typeof(T))) < 0) return;
+		throw new ConfirmAssertException(message ?? "Expected object to be negative.");
 	}
 
 	public static void ConfirmIsNotNegative<T>(this T actual, string? message = null)
 	where T : IComparable, IConvertible, IComparable<T>, IEquatable<T>
 	{
-		if (actual.CompareTo((T)Convert.ChangeType(0, typeof(T))) < 0)
-			throw new ConfirmAssertException(message ?? "Expected object to not be negative.");
+		if (actual.CompareTo((T)Convert.ChangeType(0, typeof(T))) >= 0) return;
+		throw new ConfirmAssertException(message ?? "Expected object to not be negative.");
 	}
+	#endregion
 
+	#region ConfirmIsZero
 	public static void ConfirmIsZero<T>(this T actual, string? message = null)
 	where T : IComparable, IConvertible, IComparable<T>, IEquatable<T>
 	{
-		if (actual.CompareTo((T)Convert.ChangeType(0, typeof(T))) != 0)
-			throw new ConfirmAssertException(message ?? "Expected object to be zero.");
+		if (actual.CompareTo((T)Convert.ChangeType(0, typeof(T))) == 0) return;
+		throw new ConfirmAssertException(message ?? "Expected object to be zero.");
 	}
 
 	public static void ConfirmIsNotZero<T>(this T actual, string? message = null)
 	where T : IComparable, IConvertible, IComparable<T>, IEquatable<T>
 	{
-		if (actual.CompareTo((T)Convert.ChangeType(0, typeof(T))) == 0)
-			throw new ConfirmAssertException(message ?? "Expected object to not be zero.");
+		if (actual.CompareTo((T)Convert.ChangeType(0, typeof(T))) != 0) return;
+		throw new ConfirmAssertException(message ?? "Expected object to not be zero.");
 	}
+	#endregion
 }
