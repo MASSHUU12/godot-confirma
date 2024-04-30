2f0d5413ba12b42795727a07bc6e9469c7bfe6bd
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-04-30 13:31:16 +0200

    test: write tests for ConfirmSignal extensions

---
+++ tests/ConfirmSignalTest.cs
@@ -0,0 +1,38 @@
+using Confirma.Attributes;
+using Confirma.Exceptions;
+using Godot;
+
+namespace Confirma.Tests;
+
+[TestClass]
+[Parallelizable]
+public static class ConfirmSignalTest
+{
+	private static readonly Button _button = new();
+
+	[AfterAll]
+	public static void AfterAll()
+	{
+		_button.QueueFree();
+	}
+
+	#region ConfirmSignalExists
+	[TestCase("pressed")]
+	[TestCase("toggled")]
+	[TestCase("button_down")]
+	public static void ConfirmSignalExists_WhenSignalExists(string signalName)
+	{
+		_button.ConfirmSignalExists((StringName)signalName);
+	}
+
+	[TestCase("signal_that_does_not_exist")]
+	[TestCase("signal_that_is_not_here")]
+	[TestCase("signal_that_is_nowhere_to_be_found")]
+	public static void ConfirmSignalExists_WhenSignalDoesNotExist(string signalName)
+	{
+		ConfirmExtensions.ConfirmThrows<ConfirmAssertException>(
+			() => _button.ConfirmSignalExists((StringName)signalName)
+		);
+	}
+	#endregion
+}
