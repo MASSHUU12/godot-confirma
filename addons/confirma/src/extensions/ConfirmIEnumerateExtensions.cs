6f1ced2bdb1ab3fe13d8d6cc609fa1d5fa3bb72e
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-05-04 21:03:15 +0200

    chore: make extensions from remaining methods

--- addons/confirma/src/extensions/ConfirmIEnumerateExtensions.cs
+++ addons/confirma/src/extensions/ConfirmIEnumerateExtensions.cs
@@ -56,63 +56,63 @@ public static class ConfirmIEnumerateExtensions
 		throw new ConfirmAssertException(message ?? $"Expected less than or equal to {expected} but was {actual.Count()}.");
 	}
 
-	public static void ConfirmContains<T>(IEnumerable<T> actual, T expected, string? message = null)
+	public static void ConfirmContains<T>(this IEnumerable<T> actual, T expected, string? message = null)
 	{
 		if (actual.Contains(expected)) return;
 
 		throw new ConfirmAssertException(message ?? $"Expected to contain {expected} but did not.");
 	}
 
-	public static void ConfirmNotContains<T>(IEnumerable<T> actual, T expected, string? message = null)
+	public static void ConfirmNotContains<T>(this IEnumerable<T> actual, T expected, string? message = null)
 	{
 		if (!actual.Contains(expected)) return;
 
 		throw new ConfirmAssertException(message ?? $"Expected not to contain {expected} but did.");
 	}
 
-	public static void ConfirmAllMatch<T>(IEnumerable<T> actual, Func<T, bool> predicate, string? message = null)
+	public static void ConfirmAllMatch<T>(this IEnumerable<T> actual, Func<T, bool> predicate, string? message = null)
 	{
 		if (actual.All(predicate)) return;
 
 		throw new ConfirmAssertException(message ?? "Expected all to match but did not.");
 	}
 
-	public static void ConfirmAnyMatch<T>(IEnumerable<T> actual, Func<T, bool> predicate, string? message = null)
+	public static void ConfirmAnyMatch<T>(this IEnumerable<T> actual, Func<T, bool> predicate, string? message = null)
 	{
 		if (actual.Any(predicate)) return;
 
 		throw new ConfirmAssertException(message ?? "Expected any to match but did not.");
 	}
 
-	public static void ConfirmNoneMatch<T>(IEnumerable<T> actual, Func<T, bool> predicate, string? message = null)
+	public static void ConfirmNoneMatch<T>(this IEnumerable<T> actual, Func<T, bool> predicate, string? message = null)
 	{
 		if (actual.All(x => !predicate(x))) return;
 
 		throw new ConfirmAssertException(message ?? "Expected none to match but did.");
 	}
 
-	public static void ConfirmElementsAreUnique<T>(IEnumerable<T> actual, string? message = null)
+	public static void ConfirmElementsAreUnique<T>(this IEnumerable<T> actual, string? message = null)
 	{
 		if (actual.Distinct().Count() == actual.Count()) return;
 
 		throw new ConfirmAssertException(message ?? "Expected elements to be unique but were not.");
 	}
 
-	public static void ConfirmElementsAreDistinct<T>(IEnumerable<T> actual, IEnumerable<T> expected, string? message = null)
+	public static void ConfirmElementsAreDistinct<T>(this IEnumerable<T> actual, IEnumerable<T> expected, string? message = null)
 	{
 		if (actual.Distinct().Count() == expected.Distinct().Count()) return;
 
 		throw new ConfirmAssertException(message ?? "Expected elements to be distinct but were not.");
 	}
 
-	public static void ConfirmElementsAreOrdered<T>(IEnumerable<T> actual, string? message = null)
+	public static void ConfirmElementsAreOrdered<T>(this IEnumerable<T> actual, string? message = null)
 	{
 		if (actual.OrderBy(x => x).SequenceEqual(actual)) return;
 
 		throw new ConfirmAssertException(message ?? "Expected elements to be ordered but were not.");
 	}
 
-	public static void ConfirmElementsAreInRange<T>(IEnumerable<T> actual, T from, T to, string? message = null)
+	public static void ConfirmElementsAreInRange<T>(this IEnumerable<T> actual, T from, T to, string? message = null)
 		where T : IComparable<T>
 	{
 		if (actual.All(x => x.CompareTo(from) >= 0 && x.CompareTo(to) <= 0)) return;
@@ -120,7 +120,7 @@ public static class ConfirmIEnumerateExtensions
 		throw new ConfirmAssertException(message ?? $"Expected elements to be in range [{from}, {to}] but were not.");
 	}
 
-	public static void ConfirmElementsAreEquivalent<T>(IEnumerable<T> actual, IEnumerable<T> expected, string? message = null)
+	public static void ConfirmElementsAreEquivalent<T>(this IEnumerable<T> actual, IEnumerable<T> expected, string? message = null)
 	{
 		if (actual.OrderBy(x => x).SequenceEqual(expected.OrderBy(x => x))) return;
 
