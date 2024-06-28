7d1f641b0554ebc3cb0c0e80190f113ebb9a2166
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-06-28 16:17:13 +0200

    chore: add missing default messages to the ConfirmDateTimeExtensions

--- addons/confirma/src/extensions/ConfirmDateTimeExtensions.cs
+++ addons/confirma/src/extensions/ConfirmDateTimeExtensions.cs
@@ -4,23 +4,35 @@ namespace Confirma.Extensions;
 
 public static class ConfirmDateTimeExtensions
 {
-	public static DateTime ConfirmIsBefore(this DateTime actual, DateTime dateTime)
+	public static DateTime ConfirmIsBefore(this DateTime actual, DateTime dateTime, string? message = null)
 	{
-		return actual.ConfirmLessThan(dateTime);
+		return actual.ConfirmLessThan(
+			dateTime,
+			message ?? $"Expected {actual.ToUniversalTime()} to be before {dateTime.ToUniversalTime()}."
+		);
 	}
 
-	public static DateTime ConfirmIsOnOrBefore(this DateTime actual, DateTime dateTime)
+	public static DateTime ConfirmIsOnOrBefore(this DateTime actual, DateTime dateTime, string? message = null)
 	{
-		return actual.ConfirmLessThanOrEqual(dateTime);
+		return actual.ConfirmLessThanOrEqual(
+			dateTime,
+			message ?? $"Expected {actual.ToUniversalTime()} to be on or before {dateTime.ToUniversalTime()}."
+		);
 	}
 
-	public static DateTime ConfirmIsAfter(this DateTime actual, DateTime dateTime)
+	public static DateTime ConfirmIsAfter(this DateTime actual, DateTime dateTime, string? message = null)
 	{
-		return actual.ConfirmGreaterThan(dateTime);
+		return actual.ConfirmGreaterThan(
+			dateTime,
+			message ?? $"Expected {actual.ToUniversalTime()} to be after {dateTime.ToUniversalTime()}."
+		);
 	}
 
-	public static DateTime ConfirmIsOnOrAfter(this DateTime actual, DateTime dateTime)
+	public static DateTime ConfirmIsOnOrAfter(this DateTime actual, DateTime dateTime, string? message = null)
 	{
-		return actual.ConfirmGreaterThanOrEqual(dateTime);
+		return actual.ConfirmGreaterThanOrEqual(
+			dateTime,
+			message ?? $"Expected {actual.ToUniversalTime()} to be on or after {dateTime.ToUniversalTime()}."
+		);
 	}
 }
