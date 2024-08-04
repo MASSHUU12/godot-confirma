30b7f0a7957e71015e7e05aa7e6d814adf895156
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-08-04 12:38:27 +0200

    feat: discover GDScript files

---
+++ addons/confirma/src/classes/discovery/GdTestDiscovery.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Godot;
+
+namespace Confirma.Classes.Discovery;
+
+public static class GdTestDiscovery
+{
+    public static IEnumerable<GDScript> GetTestScripts(string pathToTests)
+    {
+        foreach (string filePath in Directory.EnumerateFiles(pathToTests))
+        {
+            if (!filePath.EndsWith(".gd", StringComparison.Ordinal))
+            {
+                continue;
+            }
+
+            GDScript script = GD.Load<GDScript>(filePath);
+
+            if (!script.CanInstantiate())
+            {
+                script.Free();
+                continue;
+            }
+
+            yield return script;
+        }
+    }
+}
