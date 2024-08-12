21675ab7a4021b544245171a56c67499f2767cf5
OpalSoPL <90208316+OpalSoPL@users.noreply.github.com>
2024-08-12 15:49:52 +0200

    fix: --confirma-run runs only c# tests

--- addons/confirma/src/classes/TestManager.cs
+++ addons/confirma/src/classes/TestManager.cs
@@ -50,7 +50,7 @@ public static class TestManager
                     return;
                 }
             }
-            else {
+            else if (_props.ClassName?.Length != 0) {
                 isCs = true;
             }
 
