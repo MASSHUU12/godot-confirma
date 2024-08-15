4127635dda09cb7d0b2f2e61b7293c181e2d9c42
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-08-15 13:49:35 +0200

    feat: create enum helper class

---
+++ tests/helpers/EnumHelperTest.cs
@@ -0,0 +1,76 @@
+using System;
+using Confirma.Attributes;
+using Confirma.Extensions;
+using Confirma.Helpers;
+
+namespace Confirma.Tests;
+
+[TestClass]
+[Parallelizable]
+public static class EnumHelperTest
+{
+    [Flags]
+    private enum EEnumHelperTest : ushort
+    {
+        None = 0,
+        Value1 = 1,
+        Value2 = 1 << 1,
+        Value3 = 1 << 2,
+        Value4 = 1 << 3
+    }
+
+    [TestCase]
+    public static void TryParseFlagsEnum_ValidInput_ReturnsTrueAndParsedValue()
+    {
+        const string input = "Value1,Value2";
+
+        bool result = EnumHelper.TryParseFlagsEnum(input, out EEnumHelperTest parsed);
+
+        _ = result.ConfirmTrue();
+        _ = parsed.ConfirmEqual(EEnumHelperTest.Value1 | EEnumHelperTest.Value2);
+    }
+
+    [TestCase]
+    public static void TryParseFlagsEnum_InvalidInput_ReturnsFalseAndDefault()
+    {
+        const string input = "InvalidValue";
+
+        bool result = EnumHelper.TryParseFlagsEnum(input, out EEnumHelperTest parsed);
+
+        _ = result.ConfirmFalse();
+        _ = parsed.ConfirmEqual(EEnumHelperTest.None);
+    }
+
+    [TestCase]
+    public static void TryParseFlagsEnum_MultipleInvalidInputs_ReturnsFalseAndDefault()
+    {
+        const string input = "InvalidValue1,InvalidValue2";
+
+        bool result = EnumHelper.TryParseFlagsEnum(input, out EEnumHelperTest parsed);
+
+        _ = result.ConfirmFalse();
+        _ = parsed.ConfirmEqual(EEnumHelperTest.None);
+    }
+
+    [TestCase]
+    public static void TryParseFlagsEnum_EmptyInput_ReturnsFalseAndDefault()
+    {
+        string input = string.Empty;
+
+        bool result = EnumHelper.TryParseFlagsEnum(input, out EEnumHelperTest parsed);
+
+        _ = result.ConfirmFalse();
+        _ = parsed.ConfirmEqual(EEnumHelperTest.None);
+    }
+
+    [TestCase]
+    public static void TryParseFlagsEnum_SingleValidInput_ReturnsTrueAndParsedValue()
+    {
+        const string input = "Value1";
+
+        bool result = EnumHelper.TryParseFlagsEnum(input, out EEnumHelperTest parsed);
+
+        _ = result.ConfirmTrue();
+        _ = parsed.ConfirmEqual(EEnumHelperTest.Value1);
+    }
+}
