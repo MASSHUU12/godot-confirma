fbb61bc22dbd545e2c6e5f51c3e121e72bfca782
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-08-17 13:53:59 +0200

    feat: improve getProjectPath & create runGodot function

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -1,5 +1,11 @@
-import { $ } from "bun";
+import { $, type ShellOutput } from "bun";
 
-export async function getProjectPath(): Promise<string> {
-  return (await $`pwd`.text()).split("/").slice(0, -1).join("/");
+export function getProjectPath(): string {
+  return Bun.main.split("/").slice(0, -3).join("/");
+}
+
+export async function runGodot(...args: string[]): Promise<ShellOutput> {
+  return await $`$GODOT --path ${getProjectPath()} --headless -- ${{ raw: args.join(" ") }}`
+    .nothrow()
+    .quiet();
 }
