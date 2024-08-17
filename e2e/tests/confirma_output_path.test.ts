03e6adfb0c34df8390ee08eed5f3a00a2f874ad0
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-08-17 12:48:33 +0200

    test: improve confirma_output_path.test.ts

--- e2e/tests/confirma_output_path.test.ts
+++ e2e/tests/confirma_output_path.test.ts
@@ -5,41 +5,44 @@ import { getProjectPath } from "../utils";
 // TODO: Find a better solution
 const PATH = await getProjectPath();
 
-test("Passed empty value", async () => {
+test("Passed empty value, returns with error", async () => {
   const out =
     await $`$GODOT --path ${PATH} --headless -- --confirma-run --confirma-output-path`
       .nothrow()
       .quiet();
 
   expect(out.exitCode).toBe(1);
-  expect(out.text()).toContain("Invalid output path: .");
+  expect(out.stderr.toString()).toContain("Invalid output path: .");
 });
 
-test("Passed valid path", async () => {
+test("Passed valid path, stderr empty", async () => {
   const out =
     await $`$GODOT --path ${PATH} --headless -- --confirma-run --confirma-output-path=${PATH}/result.json`
       .nothrow()
       .quiet();
 
   expect(out.exitCode).toBe(0);
+  expect(out.stderr.toString()).toBeEmpty();
 });
 
-test("Passed invalid path", async () => {
+test("Passed invalid path, returns with error", async () => {
   const out =
     await $`$GODOT --path ${PATH} --headless -- --confirma-run --confirma-output-path=invalid`
       .nothrow()
       .quiet();
 
   expect(out.exitCode).toBe(1);
-  expect(out.text()).toContain("Invalid output path: invalid.");
+  expect(out.stderr.toString()).toContain("Invalid output path: invalid.");
 });
 
-test("Passed valid, non-existing path", async () => {
+test("Passed valid, non-existing path, returns with error", async () => {
   const out =
     await $`$GODOT --path ${PATH} --headless -- --confirma-run --confirma-output-path=./lorem/ipsum.json`
       .nothrow()
       .quiet();
 
   expect(out.exitCode).toBe(1);
-  expect(out.text()).toContain("Invalid output path: ./lorem/ipsum.json.");
+  expect(out.stderr.toString()).toContain(
+    "Invalid output path: ./lorem/ipsum.json.",
+  );
 });
