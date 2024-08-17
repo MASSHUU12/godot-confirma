a00c8a4cf4b7b6f490603b0a39ddb07e3ac5a18d
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-08-17 15:29:51 +0200

    test: add more utils for E2E testing

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -1,7 +1,15 @@
 import { $, type ShellOutput } from "bun";
 
+export const JSON_FILE_NAME = "results.json";
+export const TESTS_PATH = getE2eTestsPath() + "/";
+export const JSON_FILE_PATH = TESTS_PATH + JSON_FILE_NAME;
+
 export function getProjectPath(): string {
   return Bun.main.split("/").slice(0, -3).join("/");
+}
+
+export function getE2eTestsPath(): string {
+  return Bun.main.split("/").slice(0, -1).join("/");
 }
 
 export async function runGodot(...args: string[]): Promise<ShellOutput> {
