752571792838659b041b852cdf7f32f999056555
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-08-20 16:34:06 +0200

    feat: add 'WhenNotRunningCategory' to EIgnoreMode & allow hiding ignored tests

--- addons/confirma/src/attributes/IgnoreAttribute.cs
+++ addons/confirma/src/attributes/IgnoreAttribute.cs
@@ -1,5 +1,6 @@
 using System;
 using Confirma.Enums;
+using Confirma.Types;
 using Godot;
 using static System.AttributeTargets;
 
@@ -10,19 +11,33 @@ public class IgnoreAttribute : Attribute
 {
     public EIgnoreMode Mode { get; init; }
     public string? Reason { get; init; }
+    public bool HideFromResults { get; init; }
+    public string Category { get; init; }
 
-    public IgnoreAttribute(EIgnoreMode mode = EIgnoreMode.Always, string? reason = null)
+    public IgnoreAttribute(
+        EIgnoreMode mode = EIgnoreMode.Always,
+        string? reason = null,
+        bool hideFromResults = false,
+        string category = ""
+    )
     {
         Mode = mode;
         Reason = string.IsNullOrEmpty(reason) ? null : reason;
+        HideFromResults = hideFromResults;
+        Category = category;
     }
 
-    public bool IsIgnored()
+    public bool IsIgnored(in RunTarget target)
     {
         return Mode switch
         {
             EIgnoreMode.Always => true,
             EIgnoreMode.InEditor => Engine.IsEditorHint(),
+            EIgnoreMode.WhenNotRunningCategory => (
+                (target.Target == ERunTargetType.Category
+                && target.Name != Category)
+                || string.IsNullOrEmpty(target.Name)
+            ),
             _ => false
         };
     }
--- addons/confirma/src/enums/EIgnoreMode.cs
+++ addons/confirma/src/enums/EIgnoreMode.cs
@@ -1,7 +1,11 @@
+using System;
+
 namespace Confirma.Enums;
 
+[Flags]
 public enum EIgnoreMode
 {
     Always = 0,
-    InEditor = 1
+    InEditor = 1 << 0,
+    WhenNotRunningCategory = 1 << 1
 }
