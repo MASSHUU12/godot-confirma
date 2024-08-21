7f02a2ca2e2d23aff7b4be8f44152028f75238a6
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-08-21 18:02:29 +0200

    test: implement missing test for '--confirma-sequential' argument

---
+++ e2e/tests/dummy_tests/DummySequentialTest.cs
@@ -0,0 +1,32 @@
+using Confirma.Extensions;
+using Confirma.Attributes;
+using static Confirma.Enums.EIgnoreMode;
+
+[TestClass]
+[Parallelizable]
+[Category("DummySequentialTests")]
+[Ignore(
+    WhenNotRunningCategory,
+    category: "DummySequentialTests",
+    hideFromResults: true
+)]
+public static class DummySequentialTest
+{
+    [TestCase]
+    public static void SuperTest()
+    {
+        _ = true.ConfirmTrue();
+    }
+
+    [TestCase]
+    public static void SuperExtraTest()
+    {
+        _ = true.ConfirmTrue();
+    }
+
+    [TestCase]
+    public static void SuperExtraProTest()
+    {
+        _ = true.ConfirmTrue();
+    }
+}
