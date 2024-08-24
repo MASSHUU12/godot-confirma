3031dd6f42a7714edeb2a479a817d8948a9f0e29
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-08-24 13:02:34 +0200

    fix: correctly parse messages from GDScript to C#

--- addons/confirma/src/wrappers/WrapperBase.cs
+++ addons/confirma/src/wrappers/WrapperBase.cs
@@ -8,8 +8,23 @@ public partial class WrapperBase : CSharpScript
 {
     public static event Action<string>? GdAssertionFailed;
 
-    protected static void CallGdAssertionFailed(ConfirmAssertException e)
+    protected static void CallGdAssertionFailed(in ConfirmAssertException e)
     {
-        GdAssertionFailed?.Invoke(e.InnerException?.Message ?? e.Message);
+        GdAssertionFailed?.Invoke(
+            string.IsNullOrEmpty(e.Message)
+                ? e.InnerException?.Message ?? string.Empty
+                : e.Message
+        );
+    }
+
+    /// <summary>
+    /// 'null' in GDScript is not the same as null in C#.
+    /// That's why this 'parser' is necessary.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    protected static string? ParseMessage(string? message)
+    {
+        return string.IsNullOrEmpty(message) ? null : message;
     }
 }
