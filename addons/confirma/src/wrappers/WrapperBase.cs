62393bce8c7ac1b0eeab632add6100820b04628d
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-08-24 12:10:16 +0200

    chore: simplify WrapperBase

--- addons/confirma/src/wrappers/WrapperBase.cs
+++ addons/confirma/src/wrappers/WrapperBase.cs
@@ -1,16 +1,14 @@
 using System;
 using Confirma.Exceptions;
-using Confirma.Types;
 using Godot;
 
 namespace Confirma.Wrappers;
 
 public partial class WrapperBase : CSharpScript
 {
-    public static TestsProps Props { get; set; }
     public static event Action<string>? GdAssertionFailed;
 
-    protected void CallGdAssertionFailed(ConfirmAssertException e)
+    protected static void CallGdAssertionFailed(ConfirmAssertException e)
     {
         GdAssertionFailed?.Invoke(e.InnerException?.Message ?? e.Message);
     }
