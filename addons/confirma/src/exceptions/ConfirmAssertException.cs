0cc6535f2b7b88a8ceb2d2bc418330467fa33cc5
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-08-28 12:27:40 +0200

    feat: add constructor for ConfirmAssertException for automatic message generation

--- addons/confirma/src/exceptions/ConfirmAssertException.cs
+++ addons/confirma/src/exceptions/ConfirmAssertException.cs
@@ -1,13 +1,19 @@
 using System;
+using Confirma.Helpers;
 
 namespace Confirma.Exceptions;
 
 public class ConfirmAssertException : Exception
 {
+    private static readonly AssertionMessageGenerator _generator = new();
+
     public ConfirmAssertException(string message) : base(message) { }
 
     public ConfirmAssertException(string message, Exception innerException)
     : base(message, innerException) { }
+
+    public ConfirmAssertException(string assertion, object expected, object actual)
+    : base(_generator.GenerateAssertionMessage(assertion, expected, actual)) { }
 
     public ConfirmAssertException() { }
 }
