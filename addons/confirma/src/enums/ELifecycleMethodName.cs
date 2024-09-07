e76fc767740c19dd736abde694a5132066e57874
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-09-07 15:29:39 +0200

    feat: create ELifecycleMethodName enum & GdScriptInfo class

---
+++ addons/confirma/src/enums/ELifecycleMethodName.cs
@@ -0,0 +1,11 @@
+namespace Confirma.Enums;
+
+public enum ELifecycleMethodName : byte
+{
+    AfterAll = 0,
+    BeforeAll = 1,
+    SeUp = 2,
+    TearDown = 3,
+    Category = 4,
+    Ignore = 5
+}
