464ec2483569e96c54f64e4bfd441ed41fa93da9
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-09-07 12:05:58 +0200

    feat: add lifecycle method declarations to TestClass.gd

--- addons/confirma/src/classes/TestClass.gd
+++ addons/confirma/src/classes/TestClass.gd
@@ -1,1 +1,25 @@
 class_name TestClass
+
+
+func after_all() -> void:
+	pass
+
+
+func before_all() -> void:
+	pass
+
+
+func category() -> String:
+	return ""
+
+
+func ignore() -> PackedStringArray:
+	return []
+
+
+func set_up() -> void:
+	pass
+
+
+func tear_down() -> void:
+	pass
