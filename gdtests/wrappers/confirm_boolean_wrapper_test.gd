ea0e3e3f9c6cb7f77c844fdbdb6e1c9181977953
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-09-09 09:31:25 +0200

    test: cleanup confirm_boolean_wrapper_test.gd

--- gdtests/wrappers/confirm_boolean_wrapper_test.gd
+++ gdtests/wrappers/confirm_boolean_wrapper_test.gd
@@ -1,13 +1,5 @@
 class_name ConfirmBooleanWrapperTest extends TestClass
 
-func category() -> String:
-	return "Hi"
-
-
-func ignore() -> Ignore:
-	return Ignore.new(2, "", true, "Hi")
-
-
 func confirm_true_when_true() -> void:
 	ConfirmBoolean.is_true(true)
 
