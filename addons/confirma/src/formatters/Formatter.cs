0e1b9ae5e21f11fad0ef45642c959c8dd5b12315
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-09-21 20:46:21 +0200

    feat: add default format for formatters

--- addons/confirma/src/formatters/Formatter.cs
+++ addons/confirma/src/formatters/Formatter.cs
@@ -4,5 +4,6 @@ namespace Confirma.Formatters;
 
 public abstract class Formatter : IFormatter
 {
+    public static readonly string DefaultFormat = "Expected {1} but was {2}.";
     public abstract string Format(object value);
 }
