a81d35299c9d779c094716eae387770dee4ea40e
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-09-21 21:16:21 +0200

    feat: add new overload for ConfirmAssertException

--- addons/confirma/src/exceptions/ConfirmAssertException.cs
+++ addons/confirma/src/exceptions/ConfirmAssertException.cs
@@ -1,19 +1,51 @@
 using System;
+using Confirma.Formatters;
 using Confirma.Helpers;
 
 namespace Confirma.Exceptions;
 
 public class ConfirmAssertException : Exception
 {
-    private static readonly AssertionMessageGenerator _generator = new();
-
     public ConfirmAssertException(string message) : base(message) { }
 
     public ConfirmAssertException(string message, Exception innerException)
     : base(message, innerException) { }
 
-    public ConfirmAssertException(string assertion, object expected, object actual)
-    : base(_generator.GenerateAssertionMessage(assertion, expected, actual)) { }
+    public ConfirmAssertException(
+        string assertion,
+        object expected,
+        object actual
+    )
+    : base(
+        new AssertionMessageGenerator(
+            Formatter.DefaultFormat,
+            assertion,
+            new DefaultFormatter(),
+            expected,
+            actual
+        ).GenerateMessage()
+    )
+    { }
+
+    public ConfirmAssertException(
+        string format,
+        string assertion,
+        Formatter formatter,
+        object actual,
+        object? expected,
+        string? customMessage
+    )
+    : base(
+        customMessage
+        ?? new AssertionMessageGenerator(
+            format,
+            assertion,
+            formatter,
+            actual,
+            expected
+        ).GenerateMessage()
+    )
+    { }
 
     public ConfirmAssertException() { }
 }
