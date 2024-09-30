643128f936e021c42fc78e58429c1a77d16e7c24
OpalSoPL <90208316+OpalSoPL@users.noreply.github.com>
2024-09-30 16:30:29 +0200

    feat: add functionality to new settings panel

---
+++ addons/confirma/src/scenes/confirma_bottom_panel_options/ConfirmaBotomPanelOptions.cs
@@ -0,0 +1,48 @@
+#if TOOLS
+
+using Confirma.Enums;
+using Confirma.Scenes;
+using Godot;
+
+[Tool]
+public partial class ConfirmaBotomPanelOptions : Window
+{
+    #nullable disable
+    private CheckBox _verbose;
+    private TextEdit _category;
+    private ConfirmaAutoload _autoload;
+    #nullable restore
+
+    public override void _Ready()
+    {
+        CloseRequested += CloseRequest;
+
+        _verbose = GetNode<CheckBox>("%Verbose");
+        _category = GetNode<TextEdit>("%Category");
+
+        _ = CallDeferred("LateInit");
+    }
+
+    private void LateInit()
+    {
+        _autoload = GetNode<ConfirmaAutoload>("/root/Confirma");
+
+        _verbose.Toggled += (bool on) => _autoload.Props.IsVerbose = on;
+
+        _category.TextChanged += () =>
+        {
+            _autoload.Props.Target = _autoload.Props.Target with
+            {
+                Target = ERunTargetType.Category,
+                Name = _category.Text
+            };
+        };
+    }
+
+    void CloseRequest()
+    {
+        Hide();
+    }
+}
+
+#endif
