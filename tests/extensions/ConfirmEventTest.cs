1f4b0f41b663310a31342fbc08d4cf02c06aedf3
OpalSoPL <90208316+OpalSoPL@users.noreply.github.com>
2024-10-05 17:48:50 +0200

    test: add unit tests for `GetEventName`

--- tests/extensions/ConfirmEventTest.cs
+++ tests/extensions/ConfirmEventTest.cs
@@ -10,10 +10,12 @@ namespace Confirma.Tests;
 public static class ConfirmEventTest
 {
     private static event EventHandler<TestEventArgs>? TestEvent;
+    private static event EventHandler<TestEventArgs>? NullEvent = null;
+    private static event EventHandler<TestEventArgs>? FieldInfoEvent = (e,o) => {}; //second "unknown"
 
-    private static void OnTestEvent(TestEventArgs e)
+    private static void OnEvent(TestEventArgs e,EventHandler<TestEventArgs>? handler)
     {
-        TestEvent?.Invoke(null, e);
+        handler?.Invoke(null, e);
     }
 
     private class TestEventArgs : EventArgs { }
@@ -21,7 +23,7 @@ public static class ConfirmEventTest
     [TestCase]
     public static void ConfirmRaisesEvent_WhenTestRaises()
     {
-        Action action = static () => OnTestEvent(new());
+        Action action = static () => OnEvent(new(),TestEvent);
 
         _ = action.ConfirmRaisesEvent(ref TestEvent);
     }
@@ -39,6 +41,36 @@ public static class ConfirmEventTest
     }
 
     [TestCase]
+    public static void ConfirmRaisesEvent_WhenEventHandlerIsNull()
+    {
+        Action action = static () =>
+        {
+            Action a = static () => { /* No event raise here */ };
+            _ = a.ConfirmRaisesEvent(ref NullEvent);
+        };
+
+        action.ConfirmThrowsWMessage<ConfirmAssertException>(
+            "Assertion ConfirmRaisesEvent failed: " +
+            "Expected event null to be raised."
+        );
+    }
+
+    [TestCase]
+    public static void ConfirmRaisesEvent_WhenEventHandlerIsUnknown()
+    {
+        Action action = static () =>
+        {
+            Action a = static () => { /* No event raise here */ };
+            _ = a.ConfirmRaisesEvent(ref FieldInfoEvent);
+        };
+
+        action.ConfirmThrowsWMessage<ConfirmAssertException>(
+            "Assertion ConfirmRaisesEvent failed: " +
+            "Expected event unknown to be raised."
+        );
+    }
+
+    [TestCase]
     public static void ConfirmDoesNotRaiseEvent_WhenTestDoesNotRaise()
     {
         Action action = static () => { /* No event raise here */ };
@@ -51,10 +83,42 @@ public static class ConfirmEventTest
     {
         Action action = static () =>
         {
-            Action a = static () => OnTestEvent(new());
+            Action a = static () => OnEvent(new(),TestEvent);
             _ = a.ConfirmDoesNotRaiseEvent(ref TestEvent);
         };
 
         _ = action.ConfirmThrows<ConfirmAssertException>();
+    }
+
+    [TestCase]
+    public static void ConfirmDoesNotRaiseEvent_WhenEventHandlerIsNull()
+    {
+        Action action = static () =>
+        {
+            Action a = static () => OnEvent(new(),NullEvent);
+
+            _ = a.ConfirmDoesNotRaiseEvent(ref NullEvent);
+        };
+
+        action.ConfirmThrowsWMessage<ConfirmAssertException>(
+            "Assertion ConfirmRaisesEvent failed: " +
+            "Expected event null not to be raised."
+        );
+    }
+
+    [TestCase]
+    public static void ConfirmDoesNotRaisesEvent_WhenEventHandlerIsUnknown()
+    {
+        Action action = static () =>
+        {
+            Action a = static () => OnEvent(new(),FieldInfoEvent);
+
+            _ = a.ConfirmDoesNotRaiseEvent(ref FieldInfoEvent);
+        };
+
+        action.ConfirmThrowsWMessage<ConfirmAssertException>(
+            "Assertion ConfirmRaisesEvent failed: " +
+            "Expected event unknown not to be raised."
+        );
     }
 }
