7d664fbb60d6e25a9e13f3b1109f552fc71b7e4a
MASSHUU12 <gawrysiak.maciej@proton.me>
2024-10-11 18:18:32 +0200

    feat: create TreeContent scene

---
+++ addons/confirma/src/scenes/tree_content/TreeContent.cs
@@ -0,0 +1,36 @@
+using Godot;
+using static Godot.TreeItem;
+
+namespace Confirma.Scenes;
+
+public partial class TreeContent : Tree
+{
+    public TreeItem AddRoot()
+    {
+        return CreateItem();
+    }
+
+    public TreeItem AddLabel(string text, TreeItem? parent = null)
+    {
+        TreeItem child = CreateItem(parent);
+        child.SetText(0, text);
+        child.SetSelectable(0, false);
+        child.SetSelectable(1, false);
+
+        return child;
+    }
+
+    public TreeItem AddCheckBox(string text, TreeItem? parent = null)
+    {
+        TreeItem child = CreateItem(parent);
+        child.SetText(0, text);
+        child.SetEditable(1, true);
+        child.SetCellMode(1, TreeCellMode.Check);
+        child.SetSelectable(0, false);
+        child.SetSelectable(1, false);
+        // child.SetExpandRight(0, true);
+        // child.SetExpandRight(1, false);
+
+        return child;
+    }
+}
