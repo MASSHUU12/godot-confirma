4cf8ee220bfdb94a6fb7b04a8b670c3c51befb3e
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-10-28 18:10:35 +0100

    test: write basic tests for proxy type

---
+++ tests/classes/MockTest.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using Confirma.Attributes;
+using Confirma.Classes;
+using Confirma.Extensions;
+
+namespace Confirma.Tests;
+
+[TestClass]
+public class MockTest
+{
+    public interface ITestInterface
+    {
+        void TestMethod();
+        int TestMethodWithReturn();
+    }
+
+    [TestCase]
+    public void ProxyType_ShouldBeCreatedSuccessfully()
+    {
+        Mock<ITestInterface> mock = new();
+        Type proxyType = mock.ProxyType;
+
+        _ = proxyType.ConfirmNotNull();
+        _ = typeof(ITestInterface).IsAssignableFrom(proxyType).ConfirmTrue();
+    }
+
+    [TestCase]
+    public void ProxyType_ShouldImplementAllInterfaceMethods()
+    {
+        Mock<ITestInterface> mock = new();
+        Type proxyType = mock.ProxyType;
+
+        MethodInfo[] interfaceMethods = typeof(ITestInterface).GetMethods();
+        MethodInfo[] proxyMethods = proxyType.GetMethods();
+
+        foreach (MethodInfo iMethod in interfaceMethods)
+        {
+            _ = Confirm.IsTrue(
+                proxyMethods.Any(
+                    m => m.Name == iMethod.Name
+                    && m.ReturnType == iMethod.ReturnType
+                    && m.GetParameters().Length == iMethod.GetParameters().Length
+                ),
+                $"Method {iMethod.Name} is not implemented in proxy type."
+            );
+        }
+    }
+
+    [TestCase]
+    public void ProxyType_ShouldHaveCorrectMethodReturnTypes()
+    {
+        Mock<ITestInterface> mock = new();
+        Type proxyType = mock.ProxyType;
+
+        MethodInfo? testMethod = proxyType.GetMethod("TestMethodWithReturn");
+        Type? returnType = testMethod?.ReturnType;
+
+        _ = typeof(int).ConfirmEqual(returnType);
+        _ = mock.Instance.TestMethodWithReturn().ConfirmEqual(0);
+    }
+}
