2f10ab06b4f649f29cab68c3058a637ecf7712df
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-10-30 15:07:44 +0100

    test: add missing tests for Mock class

--- tests/classes/MockTest.cs
+++ tests/classes/MockTest.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Confirma.Attributes;
 using Confirma.Classes;
 using Confirma.Classes.Mock;
 using Confirma.Extensions;
+using Confirma.Helpers;
 
 namespace Confirma.Tests;
 
+[SetUp]
 [TestClass]
 public class MockTest
 {
@@ -18,6 +21,86 @@ public class MockTest
         string? GetString();
         object? GetObject();
         void DoSomething();
+        string StringMethod(string input);
+    }
+
+    private Mock<ITestInterface> _mock = new();
+
+    public void SetUp()
+    {
+        _mock = new();
+    }
+
+    [TestCase]
+    public void Instance_ShouldNotBeNull()
+    {
+        _ = _mock.Instance.ConfirmNotNull();
+    }
+
+    [TestCase]
+    public void Instance_ShouldImplementInterface()
+    {
+        _ = _mock.Instance.GetType()
+            .ImplementsAny<ITestInterface>()
+            .ConfirmTrue();
+    }
+
+    [TestCase]
+    public void MethodCalls_ShouldBeRecorded()
+    {
+        _mock.Instance.DoSomething();
+
+        IReadOnlyList<CallRecord> callRecords = _mock.GetCallRecords();
+
+        _ = callRecords.ConfirmCount(1);
+        _ = callRecords[0].MethodName.ConfirmEqual("DoSomething");
+        _ = callRecords[0].Arguments.ConfirmEmpty();
+    }
+
+    [TestCase]
+    public void MethodArguments_ShouldBeRecorded()
+    {
+        _ = _mock.Instance.StringMethod("test");
+
+        IReadOnlyList<CallRecord> callRecords = _mock.GetCallRecords();
+
+        _ = callRecords.ConfirmCount(1);
+        _ = callRecords[0].MethodName.ConfirmEqual("StringMethod");
+        _ = callRecords[0].Arguments.ConfirmCount(1);
+        _ = callRecords[0].Arguments[0].ConfirmEqual("test");
+    }
+
+    [TestCase]
+    public void DefaultReturnValue_ShouldBeReturned()
+    {
+        _mock.SetDefaultReturnValue("GetInt", 42);
+        _ = _mock.Instance.GetInt().ConfirmEqual(42);
+    }
+
+    [TestCase]
+    public void UndefinedMethod_ShouldThrowException()
+    {
+        _ = Confirm.Throws<ArgumentException>(
+            () => _mock.SetDefaultReturnValue("NonExistentMethod", 0)
+        );
+    }
+
+    [TestCase]
+    public void DoSomething_ShouldHandleProperly()
+    {
+        _mock.Instance.DoSomething();
+
+        IReadOnlyList<CallRecord> callRecords = _mock.GetCallRecords();
+
+        _ = callRecords.ConfirmCount(1);
+        _ = callRecords[0].MethodName.ConfirmEqual("DoSomething");
+    }
+
+    [TestCase]
+    public void MethodWithoutDefaultReturn_ShouldReturnDefault()
+    {
+        string result = _mock.Instance.StringMethod("input");
+        _ = result.ConfirmEmpty();
     }
 
     [TestCase]
