6478a75148cd45b3fd58cad0ec54d62c5be4158a
OpalSoPL <90208316+OpalSoPL@users.noreply.github.com>
2024-11-10 16:36:19 +0100

    feat: handle `link` in godot

--- addons/confirma/src/scenes/help_panel/HelpPanel.cs
+++ addons/confirma/src/scenes/help_panel/HelpPanel.cs
@@ -9,10 +9,22 @@ public partial class HelpPanel : Control
     public override void _Ready()
     {
         Log.RichOutput = GetNode<RichTextLabel>("%Output");
+        Log.RichOutput.MetaClicked += _on_meta_clicked;
 
         ConfirmaAutoload autoload = GetNodeOrNull<ConfirmaAutoload>("/root/Confirma");
 
         Help.ShowHelpPage(autoload.Props.SelectedHelpPage);
 
+    }
+
+    public void _on_meta_clicked (Variant meta)
+    {
+        switch(meta.VariantType)
+        {
+            case Variant.Type.String:
+                OS.ShellOpen((string)meta);
+                break;
+            //place for future expansion
+        }
     }
 }
