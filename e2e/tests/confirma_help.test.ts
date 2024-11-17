837905b31fadf69b345d1b71bfb00d9c8d624d77
OpalSoPL <90208316+OpalSoPL@users.noreply.github.com>
2024-11-17 13:28:07 +0100

    test(e2e): `--confirma-help`: check if requested page is correct

--- e2e/tests/confirma_help.test.ts
+++ e2e/tests/confirma_help.test.ts
@@ -2,11 +2,12 @@ import { expect, test } from "bun:test";
 import { runGodot } from "../utils";
 
 test("Passed nothing, returns help page", async() => {
-    const { exitCode, stderr } = await runGodot(
+    const { exitCode, stderr, stdout} = await runGodot(
         "--confirma-help",
       );
 
     expect(exitCode).toBe(0);
+    expect(stdout.toString()).toContain("Arguments");
     expect(stderr.toString()).toBeEmpty();
 });
 test("Passed nonexisting page, returns with error", async() => {
@@ -18,10 +19,12 @@ test("Passed nonexisting page, returns with error", async() => {
     expect(stderr.toString()).toContain("Page: `lorem`, not found or failed to load");
 });
 test("Passed existing page, returns help page", async() => {
-    const { exitCode, stderr } = await runGodot(
+    const { exitCode, stderr, stdout } = await runGodot(
         "--confirma-help=default", //todo change to non-default page
       );
 
     expect(exitCode).toBe(0);
+    expect(stdout.toString()).toContain("Arguments");
     expect(stderr.toString()).toBeEmpty();
 });
+``
