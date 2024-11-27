e393200b79186337693d84f5e76e5c506cb7ebee
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-11-27 10:30:35 +0100

    refactor(CollectionFormatter): use cache & extract methods into smaller helpers

--- addons/confirma/src/formatters/CollectionFormatter.cs
+++ addons/confirma/src/formatters/CollectionFormatter.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -10,6 +11,8 @@ namespace Confirma.Formatters;
 public class CollectionFormatter : Formatter
 {
     private readonly bool _addTypeHint;
+    private static readonly ConcurrentDictionary<Type, Type?> ElementTypeCache = new();
+    private static readonly ConcurrentDictionary<Type, MethodInfo> ToStringMethodCache = new();
 
     public CollectionFormatter(bool addTypeHint = true)
     {
@@ -25,64 +28,90 @@ public class CollectionFormatter : Formatter
 
         if (value is IEnumerable enumerable && value is not string)
         {
-            Type? elementType = value.GetType().GetInterfaces()
-                .Where(static t => t.IsGenericType
-                    && t.GetGenericTypeDefinition() == typeof(IEnumerable<>)
-                )
-                .Select(static t => t.GetGenericArguments()[0])
-                .FirstOrDefault();
-
+            Type collectionType = value.GetType();
+            Type? elementType = GetElementType(collectionType);
             if (elementType is null)
             {
-                // Fallback if unable to determine element type
-                return CollectionHelper.ToString(
-                    enumerable.Cast<object?>(),
-                    depth: 0,
-                    maxDepth: 1,
-                    addBrackets: true,
-                    addTypeHint: _addTypeHint
-                );
+                return FormatFallback(enumerable);
             }
 
-            // The signature of the CollectionHelper.ToString method
-            Type[] methodParamTypes = new Type[]
-            {
-                typeof(IEnumerable<>).MakeGenericType(elementType), // IEnumerable<T>
-                typeof(uint),                                       // depth
-                typeof(uint),                                       // maxDepth
-                typeof(bool),                                       // addBrackets
-                typeof(bool)                                        // addTypeHint
-            };
-
-            MethodInfo? toStringMethod = typeof(CollectionHelper)
-                .GetMethods(BindingFlags.Static | BindingFlags.Public)
-                .FirstOrDefault(
-                    static m => m.Name == nameof(CollectionHelper.ToString)
-                    && m.IsGenericMethod
-                );
-
-            if (toStringMethod is not null)
-            {
-                toStringMethod = toStringMethod.MakeGenericMethod(elementType);
-
-                object[] parameters = new object[]
-                {
-                    value,       // The collection
-                    (uint)0,     // depth
-                    (uint)1,     // maxDepth
-                    true,        // addBrackets
-                    _addTypeHint // addTypeHint
-                };
-
-                object? result = toStringMethod.Invoke(null, parameters);
-
-                if (result is string strResult)
-                {
-                    return strResult;
-                }
-            }
+            MethodInfo? toStringMethod = GetToStringMethod(elementType);
+            return toStringMethod is not null
+                ? InvokeToStringMethod(toStringMethod, value)
+                : new DefaultFormatter().Format(value);
         }
 
         return new DefaultFormatter().Format(value);
+    }
+
+    private static Type? GetElementType(Type collectionType)
+    {
+        if (ElementTypeCache.TryGetValue(collectionType, out Type? elementType))
+        {
+            return elementType;
+        }
+
+        elementType = collectionType.GetInterfaces()
+            .FirstOrDefault(
+                static t => t.IsGenericType
+                && t.GetGenericTypeDefinition() == typeof(IEnumerable<>)
+            )
+            ?.GetGenericArguments()[0];
+
+        ElementTypeCache[collectionType] = elementType;
+
+        return elementType;
+    }
+
+    private MethodInfo? GetToStringMethod(Type elementType)
+    {
+        if (ToStringMethodCache.TryGetValue(
+            elementType,
+            out MethodInfo? toStringMethod)
+        )
+        {
+            return toStringMethod;
+        }
+
+        MethodInfo? genericMethod = typeof(CollectionHelper)
+            .GetMethods(BindingFlags.Static | BindingFlags.Public)
+            .FirstOrDefault(
+                static m => m.Name == nameof(CollectionHelper.ToString)
+                && m.IsGenericMethod
+            );
+
+        if (genericMethod is not null)
+        {
+            toStringMethod = genericMethod.MakeGenericMethod(elementType);
+            ToStringMethodCache[elementType] = toStringMethod;
+        }
+
+        return toStringMethod;
+    }
+
+    private string FormatFallback(IEnumerable enumerable)
+    {
+        return CollectionHelper.ToString(
+            enumerable.Cast<object?>(),
+            depth: 0,
+            maxDepth: 1,
+            addBrackets: true,
+            addTypeHint: _addTypeHint
+        );
+    }
+
+    private string InvokeToStringMethod(MethodInfo toStringMethod, object value)
+    {
+        object[] parameters = new[]
+        {
+            value,       // The collection
+            (uint)0,     // depth
+            (uint)1,     // maxDepth
+            true,        // addBrackets
+            _addTypeHint // addTypeHint
+        };
+        return toStringMethod.Invoke(null, parameters) is string strResult
+            ? strResult
+            : new DefaultFormatter().Format(value);
     }
 }
