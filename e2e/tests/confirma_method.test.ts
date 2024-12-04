1f0105b84cbc109a15f422028445de8b588d1add
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-12-04 21:23:42 +0100

    test(E2E): adjust tests for recent changes

--- e2e/tests/confirma_method.test.ts
+++ e2e/tests/confirma_method.test.ts
@@ -4,66 +4,77 @@ import type { TestResult } from "../types/test_result";
 import type { BunFile } from "bun";
 
 beforeEach(async () => {
-  deleteFile(JSON_FILE_PATH);
+    deleteFile(JSON_FILE_PATH);
 });
 
 afterEach(async () => {
-  deleteFile(JSON_FILE_PATH);
+    deleteFile(JSON_FILE_PATH);
 });
 
 test("Empty '--confirma-run' and '--confirma-method', returns with error", async () => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run",
-    "--confirma-method",
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run",
+        "--confirma-method",
+    );
 
-  expect(exitCode).toBe(1);
-  expect(stderr.toString()).toContain(
-    "Invalid value: argument '--confirma-run' cannot be empty" +
-      " when using argument '--confirma-method'.\n",
-  );
+    expect(exitCode).toBe(1);
+    expect(stderr.toString()).toContain(
+        "Value for --confirma-method cannot be empty.",
+    );
+});
+
+test("Empty '--confirma-run', returns with error", async () => {
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run",
+        "--confirma-method=Lorem",
+    );
+
+    expect(exitCode).toBe(1);
+    expect(stderr.toString()).toContain(
+        "Invalid value: argument '--confirma-run' cannot be empty when using argument '--confirma-method'.",
+    );
 });
 
 test("Empty '--confirma-method', returns with error", async () => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run=DummySuccessfulTest",
-    "--confirma-method",
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run=DummySuccessfulTest",
+        "--confirma-method",
+    );
 
-  expect(exitCode).toBe(1);
-  expect(stderr.toString()).toContain(
-    "Invalid value: '--confirma-method' cannot be empty.\n",
-  );
+    expect(exitCode).toBe(1);
+    expect(stderr.toString()).toContain(
+        "Value for --confirma-method cannot be empty.",
+    );
 });
 
 test("Invalid method name, returns with error", async () => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run=DummySuccessfulTest",
-    "--confirma-method=ExtraSuperTest",
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run=DummySuccessfulTest",
+        "--confirma-method=ExtraSuperTest",
+    );
 
-  expect(exitCode).toBe(0); // TODO: Return 1.
-  expect(stderr.toString()).toContain(
-    "No test methods found with the name ExtraSuperTest.",
-  );
+    expect(exitCode).toBe(0); // TODO: Return 1.
+    expect(stderr.toString()).toContain(
+        "No test methods found with the name ExtraSuperTest.",
+    );
 });
 
 test("Valid method and class name, runs successfully", async () => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run=DummySuccessfulTest",
-    "--confirma-method=SuperTest",
-    "--confirma-output=json",
-    `--confirma-output-path=${JSON_FILE_PATH}`,
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run=DummySuccessfulTest",
+        "--confirma-method=SuperTest",
+        "--confirma-output=json",
+        `--confirma-output-path=${JSON_FILE_PATH}`,
+    );
 
-  expect(exitCode).toBe(0);
-  expect(stderr.toString()).toBeEmpty();
+    expect(exitCode).toBe(0);
+    expect(stderr.toString()).toBeEmpty();
 
-  const file: BunFile = Bun.file(JSON_FILE_PATH);
-  expect(await file.exists()).toBeTrue();
+    const file: BunFile = Bun.file(JSON_FILE_PATH);
+    expect(await file.exists()).toBeTrue();
 
-  const json: TestResult = await file.json();
-  expect(file.type).toBe("application/json;charset=utf-8");
-  expect(json.TotalTests).toBe(1);
-  expect(json.TestsPassed).toBe(1);
+    const json: TestResult = await file.json();
+    expect(file.type).toBe("application/json;charset=utf-8");
+    expect(json.TotalTests).toBe(1);
+    expect(json.TestsPassed).toBe(1);
 });
