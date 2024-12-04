384cfca41b2bfba471c9b08c495b40aa94ccd5b5
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-12-04 21:23:42 +0100

    test(E2E): adjust tests for recent changes

--- e2e/tests/confirma_output.test.ts
+++ e2e/tests/confirma_output.test.ts
@@ -3,85 +3,85 @@ import { deleteFile, JSON_FILE_PATH, runGodot } from "../utils";
 import type { BunFile } from "bun";
 
 afterEach(async (): Promise<void> => {
-  await deleteFile(JSON_FILE_PATH);
+    await deleteFile(JSON_FILE_PATH);
 });
 
 test("Passed empty output type, returns with error", async (): Promise<void> => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run",
-    "--confirma-output",
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run",
+        "--confirma-output",
+    );
 
-  expect(exitCode).toBe(1);
-  expect(stderr.toString()).toContain(
-    "Invalid value '' for '--confirma-output' argument.",
-  );
-  expect(await Bun.file(JSON_FILE_PATH).exists()).toBeFalse();
+    expect(exitCode).toBe(1);
+    expect(stderr.toString()).toContain(
+        "Value for --confirma-output cannot be empty.",
+    );
+    expect(await Bun.file(JSON_FILE_PATH).exists()).toBeFalse();
 });
 
 test("Passed empty output type with '=', returns with error", async (): Promise<void> => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run",
-    "--confirma-output=",
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run",
+        "--confirma-output=",
+    );
 
-  expect(exitCode).toBe(1);
-  expect(stderr.toString()).toContain(
-    "Invalid value '' for '--confirma-output' argument.",
-  );
-  expect(await Bun.file(JSON_FILE_PATH).exists()).toBeFalse();
+    expect(exitCode).toBe(1);
+    expect(stderr.toString()).toContain(
+        "Value for --confirma-output cannot be empty.",
+    );
+    expect(await Bun.file(JSON_FILE_PATH).exists()).toBeFalse();
 });
 
 test("Passed 'log' output type, no JSON created", async (): Promise<void> => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run",
-    "--confirma-output=log",
-    `--confirma-output-path=${JSON_FILE_PATH}`,
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run",
+        "--confirma-output=log",
+        `--confirma-output-path=${JSON_FILE_PATH}`,
+    );
 
-  expect(exitCode).toBe(0);
-  expect(stderr.toString()).toBeEmpty();
-  expect(await Bun.file(JSON_FILE_PATH).exists()).toBeFalse();
+    expect(exitCode).toBe(0);
+    expect(stderr.toString()).toBeEmpty();
+    expect(await Bun.file(JSON_FILE_PATH).exists()).toBeFalse();
 });
 
 test("Passed 'json' output type, JSON created", async (): Promise<void> => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run",
-    "--confirma-output=json",
-    `--confirma-output-path=${JSON_FILE_PATH}`,
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run",
+        "--confirma-output=json",
+        `--confirma-output-path=${JSON_FILE_PATH}`,
+    );
 
-  expect(exitCode).toBe(0);
-  expect(stderr.toString()).toBeEmpty();
+    expect(exitCode).toBe(0);
+    expect(stderr.toString()).toBeEmpty();
 
-  const file: BunFile = Bun.file(JSON_FILE_PATH);
-  expect(await file.exists()).toBeTrue();
+    const file: BunFile = Bun.file(JSON_FILE_PATH);
+    expect(await file.exists()).toBeTrue();
 
-  await file.json();
-  expect(file.type).toBe("application/json;charset=utf-8");
+    await file.json();
+    expect(file.type).toBe("application/json;charset=utf-8");
 });
 
 test("Passed 'log,json' output type, JSON created", async (): Promise<void> => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run",
-    "--confirma-output=log,json",
-    `--confirma-output-path=${JSON_FILE_PATH}`,
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run",
+        "--confirma-output=log,json",
+        `--confirma-output-path=${JSON_FILE_PATH}`,
+    );
 
-  expect(exitCode).toBe(0);
-  expect(stderr.toString()).toBeEmpty();
-  expect(await Bun.file(JSON_FILE_PATH).exists()).toBeTrue();
+    expect(exitCode).toBe(0);
+    expect(stderr.toString()).toBeEmpty();
+    expect(await Bun.file(JSON_FILE_PATH).exists()).toBeTrue();
 });
 
 test("Passed invalid output type, returns with error", async (): Promise<void> => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run",
-    "--confirma-output=xml",
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run",
+        "--confirma-output=xml",
+    );
 
-  expect(exitCode).toBe(1);
-  expect(stderr.toString()).toContain(
-    "Invalid value 'xml' for '--confirma-output' argument.",
-  );
-  expect(await Bun.file(JSON_FILE_PATH).exists()).toBeFalse();
+    expect(exitCode).toBe(1);
+    expect(stderr.toString()).toContain(
+        "Invalid value 'xml' for '--confirma-output' argument.",
+    );
+    expect(await Bun.file(JSON_FILE_PATH).exists()).toBeFalse();
 });
--- e2e/tests/confirma_output_path.test.ts
+++ e2e/tests/confirma_output_path.test.ts
@@ -2,53 +2,57 @@ import { expect, test } from "bun:test";
 import { JSON_FILE_PATH, runGodot } from "../utils";
 
 test("Passed empty value, returns with error", async () => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run",
-    "--confirma-output-path",
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run",
+        "--confirma-output-path",
+    );
 
-  expect(exitCode).toBe(1);
-  expect(stderr.toString()).toContain("Invalid output path: .");
+    expect(exitCode).toBe(1);
+    expect(stderr.toString()).toContain(
+        "Value for --confirma-output-path cannot be empty.",
+    );
 });
 
 test("Passed empty value with '=', returns with error", async () => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run",
-    "--confirma-output-path=",
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run",
+        "--confirma-output-path=",
+    );
 
-  expect(exitCode).toBe(1);
-  expect(stderr.toString()).toContain("Invalid output path: .");
+    expect(exitCode).toBe(1);
+    expect(stderr.toString()).toContain(
+        "Value for --confirma-output-path cannot be empty.",
+    );
 });
 
 test("Passed valid path, stderr empty", async () => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run",
-    `--confirma-output-path=${JSON_FILE_PATH}`,
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run",
+        `--confirma-output-path=${JSON_FILE_PATH}`,
+    );
 
-  expect(exitCode).toBe(0);
-  expect(stderr.toString()).toBeEmpty();
+    expect(exitCode).toBe(0);
+    expect(stderr.toString()).toBeEmpty();
 });
 
 test("Passed invalid path, returns with error", async () => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run",
-    "--confirma-output-path=invalid",
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run",
+        "--confirma-output-path=invalid",
+    );
 
-  expect(exitCode).toBe(1);
-  expect(stderr.toString()).toContain("Invalid output path: invalid.");
+    expect(exitCode).toBe(1);
+    expect(stderr.toString()).toContain("Invalid output path: invalid.");
 });
 
 test("Passed valid, non-existing path, returns with error", async () => {
-  const { exitCode, stderr } = await runGodot(
-    "--confirma-run",
-    "--confirma-output-path=./lorem/ipsum.json",
-  );
+    const { exitCode, stderr } = await runGodot(
+        "--confirma-run",
+        "--confirma-output-path=./lorem/ipsum.json",
+    );
 
-  expect(exitCode).toBe(1);
-  expect(stderr.toString()).toContain(
-    "Invalid output path: ./lorem/ipsum.json.",
-  );
+    expect(exitCode).toBe(1);
+    expect(stderr.toString()).toContain(
+        "Invalid output path: ./lorem/ipsum.json.",
+    );
 });
