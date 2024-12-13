fdbd560eb045064ba181c0cb1e73e87a372fc259
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-12-13 19:09:24 +0100

    feat: create RadixNode class

---
+++ addons/confirma/src/dictionary/RadixNode.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace Confirma.Dictionary;
+
+public class RadixNode<T>
+{
+    public string Prefix { get; set; }
+    public T? Value { get; set; }
+    public Dictionary<string, RadixNode<T>> Children { get; set; }
+
+    public RadixNode(string prefix)
+    {
+        Prefix = prefix;
+        Children = new Dictionary<string, RadixNode<T>>();
+    }
+
+    public override string? ToString()
+    {
+        return $"RadixNode(Prefix=\"{Prefix}\", Value={Value})";
+    }
+}
